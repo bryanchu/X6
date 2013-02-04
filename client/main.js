42382c45f94321cbfbc0d925379eec449197ad25
Bryan Chu <bryanchu08@yahoo.com>
2013-02-03 23:43:33 -0800

    moving spaceship

--- client/main.js
+++ client/main.js
@@ -16,6 +16,7 @@ X6 = {};
 //SINGLETONS
 X6.GlobalControl = function() {
     var renderer, camera,
+        midX = window.innerWidth / 2, midY = window.innerHeight / 2,
         COLORENUM = {Red: 0xFF0000,
                     RedHighlight: 0xFF5252,
                     Orange: 0xFF8600,
@@ -29,7 +30,10 @@ X6.GlobalControl = function() {
                     Yellow: 0xFAFF6B,
                     Green: 0x00DE1A};
     var self = {};
+    self.waitingAJAXCalls = 0,
     self.initScene = function() {
+        X6.Navigation.altBar = document.getElementById('altBar');
+        X6.Navigation.altHeight = parseInt(getComputedStyle(document.getElementById('altitude')).getPropertyValue('height').replace('px', ''));
         // PB.AudioManager.sounds.themeSound.addEventListener('ended', function() {
         //     this.currentTime = 0;
         //     this.themeSound.play();
@@ -59,9 +63,15 @@ X6.GlobalControl = function() {
         //Load the meshes.
         var baseURL = "meshes/";
         var loader = new THREE.JSONLoader();
-        loader.load(baseURL + "xwing.js", createBlender);
+        self.xWing = new X6.XWing();
+        loadAndTrack("xwing.js", function(geometry) {self.xWing.addPiece(geometry, "defaultGrey");});
 
-        // Light
+        function loadAndTrack(url, func) {
+            self.waitingAJAXCalls++;
+            loader.load(baseURL + url, func);
+        };
+
+        // Lights
         light = new THREE.DirectionalLight( 0xFFFFFF );
         light.position.set( 0, 700, 220 );
         light.target.position.copy( self.scene.position );
@@ -74,18 +84,20 @@ X6.GlobalControl = function() {
         self.scene.add( light );
         
         // Camera
-        camera = new THREE.PerspectiveCamera(
+        self.camera = new THREE.PerspectiveCamera(
             70,
             window.innerWidth / window.innerHeight,
             1,
             5000000
         );
-        camera.position.set( 0, 800, 300 );
-        camera.lookAt( self.scene.position );
-        self.scene.add( camera );
+        var camY = 1,
+            camZ = -4;
+        self.camera.position.set( 0, camY, camZ );
+        // self.camera.lookAt( self.scene.position );
+        // self.scene.add( self.camera );
 
         //trackball controls
-        controls = new THREE.TrackballControls(camera, container);
+        controls = new THREE.TrackballControls(self.camera, container);
         controls.dynamicDampingFactor = 0.1;
         controls.staticMoving = true;
         controls.rotateSpeed = 2.0;
@@ -95,28 +107,6 @@ X6.GlobalControl = function() {
         controls.noPan = false;
         controls.keys = [65, 83, 68];
 
-        function loadMesh(config, url) {
-            return loader.load(config, baseURL + url, createBlender);
-        }
-
-        // function storeMesh(config, url, name) {
-        //     loader.load(config, baseURL + url, function(geometry, config) {
-        //         waitingAJAXCalls -= 1;
-        //         animMeshes[name] = createBlender(geometry, config);
-        //         initAnim();
-        //     });
-        // }
-
-        function createBlender(geometry, config) {
-            geometry.mergeVertices();
-            var Meshtype = config.meshType == "Lambert" ? THREE.MeshLambertMaterial : (config.meshType == "Basic" ? THREE.MeshBasicMaterial : THREE.MeshPhongMaterial);
-            var material = new Meshtype({specular: 0x888888, color: config.color});// map: THREE.ImageUtils.loadTexture("/img/redTest.png")});
-            var mesh = new THREE.Mesh( geometry, material );
-            mesh.useQuaternion = true;//config.useQuat;
-            mesh.scale.set(50, 50, 50);
-            X6.GlobalControl.scene.add(mesh);
-            return mesh;
-        }
         //skybox
         var urls = [
           'img/space_right1.png',
@@ -135,31 +125,112 @@ X6.GlobalControl = function() {
             uniforms: shader.uniforms,
             side: THREE.BackSide,
         });
-        var skybox = new THREE.Mesh( new THREE.CubeGeometry( 51200, 51200, 51200 ), material );
+        var skybox = new THREE.Mesh( new THREE.CubeGeometry( 51200000, 51200000, 51200000 ), material );
         self.scene.add(skybox);
 
-        // initControls();
+        initControls();
 
-        requestAnimFrame(main);
+        (function checkAjaxFinish() {
+            if (self.waitingAJAXCalls == 0) {
+                self.xWing.pieces[0].add(self.camera);
+                // self.scene.add(self.camera);
+                requestAnimFrame(main);
+            } else {
+                setTimeout(checkAjaxFinish, 200);
+            } 
+        })();
+        
+    };
+    var nextRotX = 0, nextRotY = 0;
+    function initControls() {
+        document.addEventListener('mousemove', function(event) {
+            var x = event.layerX,
+                y = event.layerY;
+            nextRotX = .0001 * (x - midX);
+            nextRotY = .0001 * (y - midY);
+        });
     };
 
     function render() {
-        renderer.render(self.scene, camera);
+        renderer.render(self.scene, self.camera);
     };
 
     function main() {
-        updatePhysics();
+        updateScene();
         controls.update();
         render();
         window.requestAnimFrame(main);
     };
 
-    function updatePhysics() {
+    function updateScene() {
+        // self.camera.position.set(pos.x, pos.y, pos.z - 1400);
+        // self.camera.lookAt(pos);
+        // console.log(nextRotX);
+        // console.log(nextRotY);
+        self.xWing.rotatePieces(nextRotX, nextRotY, 0);
+        var currRot = self.xWing.pieces[0].rotation,
+            speed = self.xWing.normalSpeed;
+        // self.xWing.movePieces(speed * Math.cos(currRot.x), speed * Math.sin(currRot.y), speed * Math.sin(currRot.x));
+        //down/up, back/forth, left/right
+        self.xWing.movePieces(speed * Math.sin(currRot.y), 
+                              speed * -Math.sin(currRot.x),
+                              speed * Math.cos(currRot.x));
+        X6.Navigation.moveAltBar(self.xWing.pieces[0].rotation.y);
     };
 
     return self;
 }();
+X6.Navigation = function() {
+    var self = {};
+    self.moveAltBar = function(rot) {
+        self.altBar.style.bottom = ((rot - (Math.PI / 2)) / -Math.PI) * parseInt(self.altHeight);
+    };
+    return self;
+}();
+//CLASSES
+X6.StarShip = function() {
+    this.pieces = [];
+};
+X6.StarShip.prototype.addPiece = function(geometry, material) {
+    geometry.mergeVertices();
+    // var Meshtype = config.meshType == "Lambert" ? THREE.MeshLambertMaterial : (config.meshType == "Basic" ? THREE.MeshBasicMaterial : THREE.MeshPhongMaterial);
+    var material = new THREE.MeshPhongMaterial({specular: 0x888888, color: this.COLORMAP[material]});// map: THREE.ImageUtils.loadTexture("/img/redTest.png")});
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.rotation.z = Math.PI / 2;
+    mesh.rotation.x = Math.PI / 2;
+    // mesh.useQuaternion = true;//config.useQuat;
+    mesh.scale.set(500, 500, 500);
+    X6.GlobalControl.scene.add(mesh);
+    this.pieces.push(mesh);
+    X6.GlobalControl.waitingAJAXCalls--;
+};
+X6.StarShip.prototype.movePieces = function(x, y, z) {
+    for (var i = 0; i < this.pieces.length; i++) {
+        var pos = this.pieces[i].position;
+        pos.x += x;
+        pos.y += y;
+        pos.z += z;
+    }
+};
+X6.StarShip.prototype.rotatePieces = function(x, y, z) {
+    for (var i = 0; i < this.pieces.length; i++) {
+        var rot = this.pieces[i].rotation;
+        
+        rot.x += x;
+        ((rot.y < Math.PI / 2 || y < 0) && (rot.y > -Math.PI / 2 || y > 0)) && (rot.y += y);
+        // rot.y += y;
+        rot.z += z;
+    }
+};
+X6.XWing = function() {
+    self = new X6.StarShip();
+    self.COLORMAP = {
+        defaultGrey: 0xcccccc
+    };
+    self.normalSpeed = 5000;
+    return self;
+};
+X6.XWing.prototype = X6.StarShip.prototype;
 
 
- 
 window.onload = X6.GlobalControl.initScene;
